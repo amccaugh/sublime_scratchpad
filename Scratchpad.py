b051575b49ff0ff0f86b7f7b0f1e122168dd37d6
Adam McCaughan <amccaugh@gmail.com>
2019-04-30 11:06:00 -0600

    Update Scratchpad.py

--- Scratchpad.py
+++ Scratchpad.py
@@ -30,9 +30,9 @@ def checkAndFillEmpty(scratchpadFile):
       scratchFile.write(headerText)
 
 def putTimeStamp(scratchpadFile):
-  timeStamp = "\n\n" + strftime("%c") + " : " + "\n" +"========================" + "\n"
-  with open(scratchpadFile, "a", encoding="utf-8") as scratchFile:
-      scratchFile.write(timeStamp)
+  # timeStamp = "\n\n" + strftime("%c") + " : " + "\n" +"========================" + "\n"
+  # with open(scratchpadFile, "a", encoding="utf-8") as scratchFile:
+  #     scratchFile.write(timeStamp)
   with open(scratchpadFile, encoding="utf-8") as scratchFile:
     count = sum(1 for line in scratchFile)
   return count
